4bc706dcaf01959d735649d78351c5bb79cb3263
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-04-30 14:20:10 +0200

    Extension criteria met

--- csharp-scrabble-challenge.Main/Program.cs
+++ csharp-scrabble-challenge.Main/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using csharp_scrabble_challenge.Main;
 
-Scrabble s = new Scrabble("street");
+// The following will first give triple letter score to 'e' and then double the total word score => 16
+Scrabble s = new Scrabble("{str[e]et}");
 Console.WriteLine(s.score());
--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -11,32 +11,13 @@ namespace csharp_scrabble_challenge.Main
     {
         string word;
         private Dictionary<char, int> letterValues = new Dictionary<char, int>(){
-            {'A', 1},
-            {'E', 1},
-            {'I', 1},
-            {'O', 1},
-            {'U', 1},
-            {'L', 1},
-            {'N', 1},
-            {'R', 1},
-            {'S', 1},
-            {'T', 1},
-            {'D', 2},
-            {'G', 2},
-            {'B', 3},
-            {'C', 3},
-            {'M', 3},
-            {'P', 3},
-            {'F', 4},
-            {'H', 4},
-            {'V', 4},
-            {'W', 4},
-            {'Y', 4},
+            {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1},
+            {'D', 2}, {'G', 2},
+            {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3},
+            {'F', 4}, {'H', 4}, {'V', 4}, {'W', 4}, {'Y', 4},
             {'K', 5},
-            {'J', 8},
-            {'X', 8},
-            {'Q', 10},
-            {'Z', 10}
+            {'J', 8}, {'X', 8},
+            {'Q', 10}, {'Z', 10}
         };
 
         public Scrabble(string word)
@@ -52,8 +33,41 @@ namespace csharp_scrabble_challenge.Main
                 char letter = this.word.ElementAt(i);
                 if (this.letterValues.ContainsKey(letter))
                 {
-                    score += this.letterValues[letter];
+                    // Check if letter is not first or last letter of the word
+                    if (i > 0 && i < this.word.Length - 1)
+                    {
+                        if (this.word.ElementAt(i - 1) == '{' && this.word.ElementAt(i + 1) == '}')
+                        {
+                            // Double letter score, example for 'o' in d{o}g
+                            score += this.letterValues[letter] * 2;
+                        }
+                        else if (this.word.ElementAt(i - 1) == '[' && this.word.ElementAt(i + 1) == ']')
+                        {
+                            // Triple letter score, example for 'o' in d[o]g
+                            score += this.letterValues[letter] * 3;
+                        }
+                        else
+                        {
+                            score += this.letterValues[letter];
+                        }
+                    }
+                    else
+                    {
+                        score += this.letterValues[letter];
+                    }
+
                 }
+            }
+
+            // Check for double word
+            if (this.word.StartsWith("{") && this.word.EndsWith("}"))
+            {
+                score *= 2;
+            }
+            // Check for triple word
+            else if (this.word.StartsWith("[") && this.word.EndsWith("]"))
+            {
+                score *= 3;
             }
             return score;
         }
