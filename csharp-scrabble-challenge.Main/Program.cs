1f14724e83a20ff94b20151edd96be628cbef8d5
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-04-30 13:42:20 +0200

    Core criteria met

--- csharp-scrabble-challenge.Main/Program.cs
+++ csharp-scrabble-challenge.Main/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using csharp_scrabble_challenge.Main;
+
+Scrabble s = new Scrabble("street");
+Console.WriteLine(s.score());
--- csharp-scrabble-challenge.Main/Scrabble.cs
+++ csharp-scrabble-challenge.Main/Scrabble.cs
@@ -9,15 +9,53 @@ namespace csharp_scrabble_challenge.Main
 {
     public class Scrabble
     {
+        string word;
+        private Dictionary<char, int> letterValues = new Dictionary<char, int>(){
+            {'A', 1},
+            {'E', 1},
+            {'I', 1},
+            {'O', 1},
+            {'U', 1},
+            {'L', 1},
+            {'N', 1},
+            {'R', 1},
+            {'S', 1},
+            {'T', 1},
+            {'D', 2},
+            {'G', 2},
+            {'B', 3},
+            {'C', 3},
+            {'M', 3},
+            {'P', 3},
+            {'F', 4},
+            {'H', 4},
+            {'V', 4},
+            {'W', 4},
+            {'Y', 4},
+            {'K', 5},
+            {'J', 8},
+            {'X', 8},
+            {'Q', 10},
+            {'Z', 10}
+        };
+
         public Scrabble(string word)
-        {            
-            //TODO: do something with the word variable
+        {
+            this.word = word.Trim().ToUpper();
         }
 
         public int score()
         {
-            //TODO: score calculation code goes here
-            throw new NotImplementedException(); //TODO: Remove this line when the code has been written
+            int score = 0;
+            for (int i = 0; i < this.word.Length; i++)
+            {
+                char letter = this.word.ElementAt(i);
+                if (this.letterValues.ContainsKey(letter))
+                {
+                    score += this.letterValues[letter];
+                }
+            }
+            return score;
         }
     }
 }
